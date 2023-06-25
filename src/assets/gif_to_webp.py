d2b70f0c34f5992b48455f9b2ff23b9d790c4c8a
robert manteghi <unruffled.nightingale@gmail.com>
2023-06-25 22:16:17 +0100

    removing unneeded gifs and adding bordle.webp

--- src/assets/gif_to_webp.py
+++ src/assets/gif_to_webp.py
@@ -1,13 +1,9 @@
 from PIL import Image
+import sys
+
+gif_file = sys.argv[1].split(".")[0]
 
 
-im = Image.open("emoji-canvas.gif")
-im.save("emoji-canvas.webp", format="WEBP", save_all=True)
-im = Image.open("esr-demo.gif")
-im.save("esr-demo.webp", format="WEBP", save_all=True)
-im = Image.open("rolling-gallery.gif")
-im.save("rolling-gallery.webp", format="WEBP", save_all=True)
-im = Image.open("tracklift.gif")
-im.save("tracklift.webp", format="WEBP", save_all=True)
-im = Image.open("treasure-hunt.gif")
-im.save("treasure-hunt.webp", format="WEBP", save_all=True)
+print(gif_file)
+im = Image.open(f"{gif_file}.gif")
+im.save(f"{gif_file}.webp", format="WEBP", save_all=True)
